e3e4817a5c4331ee8215e5c7f054a225882d84b4
JD <styelz@users.noreply.github.com>
2025-09-07 05:11:29 +1000

    Bump version to 1.7.3 and update changelog with new features, improvements, and bug fixes

--- setupCMD.py
+++ setupCMD.py
@@ -11,7 +11,7 @@ buildOptions = dict(include_files = [('dwarf_ble_connect/','./dwarf_ble_connect'
 base = 'Win32GUI' if sys.platform=='win32' else None
 setup(
     name = "Atro_Dwarf_Session",
-    version = "1.7.2",
+    version = "1.7.3",
     description = "automatic Astro Session for the Dwarf",
     options = dict(build_exe = buildOptions),
     executables = [Executable("astro_dwarf_scheduler.py",target_name="astro_dwarf_scheduler")]
--- setupUI.py
+++ setupUI.py
@@ -16,7 +16,7 @@ base = 'Win32GUI' if sys.platform == 'win32' else None
 # Setup function
 setup(
     name="Astro Dwarf Scheduler",
-    version="1.7.2",
+    version="1.7.3",
     description="Dwarf Astro Scheduler",
     options=dict(build_exe=buildOptions),
     executables=[
